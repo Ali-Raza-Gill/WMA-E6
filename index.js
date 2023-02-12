8f13f4367084fed3cdccca89758e67e5a2c10a93
Ali Raza <aliraza137725@gmail.com>
2023-02-12 23:10:40 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,136 @@
+// Assignment 8
+// if else condition practice 
+
+// Question no 1
+// let n1=20;
+// let n2=30;
+// if(n1>n2){
+//     console.log("Number 1 is greater than number 2");
+// }
+// else{
+//     console.log("Number 2 is greater than number 1");   
+// }
+
+// Question no 2
+// let num1=20;
+// let num2=30;
+// let num3=40;
+// if(num1>num2&&num1>num3){
+//     console.log("Number 1 is greater");
+// }
+// else if (num2>num1&&num2>num3){
+//     console.log("Number 2 is greater");
+// }else{
+//     console.log("Number 3 is greater");
+// }
+
+// Question no 3
+// // let number=prompt("Enter the number");
+// let number=21;
+
+// if(number>0){
+//     console.log("You entered a positive number");
+// }
+// else if(number<0){
+//     console.log("You entered a negative number");
+// }
+// else if (number==0){
+//     console.log("You entered zero");
+// }else {
+//     console.log("You enter invalid number");
+// }
+
+
+// Question no 4
+// let Number=22;
+// if(Number%5==0){
+//     console.log("Number is divisible by 5");
+// }else if(Number%11==0){
+//     console.log("Number is divisible by 11");
+// }else {
+//     console.log("Number is not divisible by given numbers");
+// }
+
+
+// Question no 5
+// let number=91;
+// if(number%2==0){
+//     console.log("Your number is even");
+// }else if(number%2==1){
+//     console.log("Your number is odd");
+// }else{
+//     console.log("You entered invalid number");
+// }
+
+// Question no 6
+// let year=2023;
+// if(year%4==0){
+//     console.log("This is leap year");
+// }else{
+//     console.log("This year is not leap year");
+// }
+
+// Question no 7
+// let alphabet='u';
+// if(alphabet=='a'||alphabet=='e'||alphabet=='i'||alphabet=='o'||alphabet=='u'){
+//     console.log("You enter a Vowel");
+// }else{
+//     console.log("You enter a consonant");
+// }
+
+// Question no 8
+// let char='A';
+// if(char.toLowerCase()==char){
+//     console.log("You enter a lowercasse letter");
+// }else if(char.toUpperCase()==char){
+//     console.log("You enter uppercase letter");
+// }else{
+//     console.log("invalid character");
+// }
+
+
+// Question no 9
+// let day=4;
+// // enter numbers from 1 to 7
+// if(day===1){
+//     console.log("Monday");
+// }
+// if(day===2){
+//     console.log("Turesday");
+// }
+// if(day===3){
+//     console.log("Wednesday");
+// }
+// if(day===4){
+//     console.log("Thursday");
+// }
+// if(day===5){
+//     console.log("Friday");
+// }if(day===6){
+//     console.log("Saturday");
+// }if(day===7){
+//     console.log("Sunday");
+// }else{
+//     console.log("you enter invalid number");
+// }
+
+
+// Question no 10
+// let month=6;
+// if(month==1||month==3||month==5||month==7||month==8||month==10||month==12){
+//     console.log("This month has 31 days");
+// }else if(month==2){
+//     console.log("This month has 28 days");
+// }else if(month==4||month==6||month==9||month==11){
+//     console.log("This month has 30 days");
+// }
+
+
+// Question no 11
+let amount=29900;
+if(){
+
+}
+
+
+
